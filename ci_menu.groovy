53cbc39ad70e8a73525e13290b115637b726d44e
Pau <paulina.lozano.g@gmail.com>
2021-09-08 20:04:46 -0500

    Commit 3

--- ci_menu.groovy
+++ ci_menu.groovy
@@ -2,11 +2,13 @@ node
 {
     timestamps
     {
-        stage('main')
-        {
-            echo "hola que hace"
+        stage('WA_setup')
+        { 
+            sh 'rm -rf menu'
             sh 'git clone https://github.com/paulozanogmz/menu.git'
-        
+            sh 'cd menu'
+            sh 'git checkout app'
+            sh 'git log --oneline -10'
         }
     }
 }
