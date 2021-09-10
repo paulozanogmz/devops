926fbc6d71465140614cde16daa339ca7a1bb9fe
Pau <paulina.lozano.g@gmail.com>
2021-09-09 19:46:00 -0500

    Commit 11 - add stage run_unit_test

--- ci_menu.groovy
+++ ci_menu.groovy
@@ -7,12 +7,16 @@ node
             sh 'rm -rf menu'
             sh 'git clone https://github.com/paulozanogmz/menu.git'
             sh 'cd menu && git checkout app'
-            sh 'cd menu && git log --oneline -10'
+            sh 'cd menu && git log --oneline -15'
         }
         stage('Run_app')
         {
             sh 'cd menu && python3 welcome.py'
             sh 'cd menu && python3 welcome.py >> output.txt'
+        }
+        stage('Run_unit_test')
+        {
+            sh 'cd menu && git checkout unit_test'
         }
     }
 } 
