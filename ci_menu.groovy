c59b880227ec6c3c273b7a045902c15112250aa4
Pau <paulina.lozano.g@gmail.com>
2021-09-09 19:50:04 -0500

    Commit 13 - added lines 20 & 21

--- ci_menu.groovy
+++ ci_menu.groovy
@@ -17,6 +17,8 @@ node
         stage('Run_unit-test')
         {
             sh 'cd menu && git checkout unit-test'
+            sh 'cd menu && python3 test_all.py'
+            sh 'cd menu && python3 test_all.py >> output_unit-test.txt'
         }
     }
 } 
